cdd9c2b551ad601d0bc1b21b0de7a7f2044e1792
dmsergio <sergiodm.1989@gmail.com>
2022-08-23 19:01:57 +0200

    [ADD] Redis example with async iterators

---
+++ 05_async_iterators/async_iterator_redis_example.py
@@ -0,0 +1,34 @@
+import asyncio
+
+import aioredis
+
+
+async def main():
+    redis = await aioredis.from_url("localhost")
+    keys = ["Americas", "Africa", "Europe", "Asia"]
+
+    async for value in OneAtTime(redis, keys):
+        ...
+        # await do_something_with(value)
+
+
+class OneAtTime:
+    def __init__(self, redis, keys):
+        self.redis = redis
+        self.keys = keys
+
+    def __aiter__(self):
+        self.ikeys = iter(self.keys)
+        return self
+
+    async def __anext__(self):
+        try:
+            k = next(self.ikeys)
+        except StopIteration:
+            raise StopAsyncIteration
+
+        value = await self.redis.get(k)
+        return value
+
+
+asyncio.run(main())
