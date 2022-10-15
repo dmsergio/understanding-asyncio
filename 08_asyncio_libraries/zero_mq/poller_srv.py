9c32022f7f087576546867e1b05539a4632ba2e7
dmsergio <sergiodm.1989@gmail.com>
2022-10-15 18:52:30 +0200

    [ADD] Case study: Multiple sockets with ZeroMQ library

---
+++ 08_asyncio_libraries/zero_mq/poller_srv.py
@@ -0,0 +1,16 @@
+import itertools
+import time
+import zmq
+
+
+context = zmq.Context()
+pusher = context.socket(zmq.PUSH)
+pusher.bind("tcp://*:5557")
+
+publisher = context.socket(zmq.PUB)
+publisher.bind("tcp://*:5556")
+
+for i in itertools.count():
+    time.sleep(1)
+    pusher.send_json(i)
+    publisher.send_json(i)
